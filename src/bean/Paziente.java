02278b68a65153207a02d365fac18595620f8044
Sara <>
2019-12-11 10:40:24 +0100

    Creazione Bean Paziente

---
+++ src/bean/Paziente.java
@@ -0,0 +1,82 @@
+package bean;
+
+import java.util.Date;
+
+public class Paziente {
+	
+	private String codiceFiscale, nome, cognome, sesso, email, residenza, password;
+	private Date dataDiNascita;
+	private Boolean attivo;
+	
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+	
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getSesso() {
+		return sesso;
+	}
+	
+	public void setSesso(String sesso) {
+		this.sesso = sesso;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getResidenza() {
+		return residenza;
+	}
+	
+	public void setResidenza(String residenza) {
+		this.residenza = residenza;
+	}
+	
+	public Date getDataDiNascita() {
+		return dataDiNascita;
+	}
+	
+	public void setDataDiNascita(Date dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean isAttivo() {
+		return attivo;
+	}
+
+	public void setAttivo(Boolean attivo) {
+		this.attivo = attivo;
+	}
+}
