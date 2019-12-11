6db3482f8834dc62be974e567b9b03c69b6327d7
Eugenio <>
2019-12-11 22:58:20 +0100

    modifica Amministratore model

--- src/model/AmministratoreModel.java
+++ src/model/AmministratoreModel.java
@@ -23,19 +23,19 @@ public class AmministratoreModel {
 	 * @return dati dell'amministratore se le credenziali sono corrette, null altrimenti
 	 */
 	public static Amministratore checkLogin(String codiceFiscale, String password) {
-		MongoCollection<Document> pazienti = DriverConnection.getConnection().getCollection("Paziente");
+		MongoCollection<Document> amministratori = DriverConnection.getConnection().getCollection("Amministratore");
 		Amministratore amministratore = null;
 		BasicDBObject andQuery = new BasicDBObject();
 		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
 		obj.add(new BasicDBObject("CodiceFiscale", codiceFiscale));
 		obj.add(new BasicDBObject("Password", password));
 		andQuery.put("$and", obj);
-		Document datiPaziente = pazienti.find(andQuery).first();
-		if(datiPaziente != null) {
+		Document datiAmministratore = amministratori.find(andQuery).first();
+		if(datiAmministratore != null) {
 			amministratore = new Amministratore();
 			amministratore.setCodiceFiscale(codiceFiscale);
-			amministratore.setNome(datiPaziente.getString("Nome"));
-			amministratore.setCognome(datiPaziente.getString("Cognome"));
+			amministratore.setNome(datiAmministratore.getString("Nome"));
+			amministratore.setCognome(datiAmministratore.getString("Cognome"));
 			amministratore.setEmail("Email");
 			
 		}
