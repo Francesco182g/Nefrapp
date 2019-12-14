a26acc29b65091b6e8bdb4bcd3b98c8c14f83e91
AnItalianMan <>
2019-12-14 22:39:50 +0100

    Aggiunta gestione medico

---
+++ src/control/GestioneMedico.java
@@ -0,0 +1,75 @@
+package control;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import bean.Paziente;
+import model.PazienteModel;
+
+/**
+ * @author Antonio
+ * Servlet implementation class GestioneMedico
+ */
+@WebServlet("/GestioneMedico")
+public class GestioneMedico extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Verifica del tipo di chiamata alla servlet (sincrona o asinconrona)(sincrona ok)
+		try {
+			if("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+				response.setContentType("application/json");
+				response.setHeader("Cache-Control", "no-cache");
+				response.getWriter().write(new Gson().toJson("Errore generato dalla richiesta!"));
+				return;
+			}
+			
+			String operazione = request.getParameter("operazione");
+			
+			if(operazione == "ResetPassword") {
+				//cose da fare
+			}else if(operazione ==  "ModificaDatiPersonali") {
+				//cose da fare
+			}else if (operazione == "VisualizzaPazientiSeguiti") {
+				visualizzaPazientiseguiti(request);
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("view/listaPazientiView"); //reindirizzamento view per la visualizzazione della lista pazienti
+				dispatcher.forward(request, response);
+				
+			}else {
+				throw new Exception("Operazione invalida");
+			}	
+		} catch (Exception e) {
+			System.out.println("Errore in gestione parametri:");
+			e.printStackTrace();		
+		}
+		
+		return;	
+	}
+
+	/**
+	 * Metodo che prende i pazienti seguiti dal medico e li inserisce nella richiesta
+	 * @param request richiesta utilizzata per ottenere parametri e settare attributi
+	 */
+	private void visualizzaPazientiseguiti(HttpServletRequest request) {
+		String codiceFiscaleMedico = request.getParameter("codiceFiscale");
+		ArrayList<Paziente> pazientiSeguiti = PazienteModel.getPazientiSeguiti(codiceFiscaleMedico);
+		request.setAttribute("pazientiSeguiti", pazientiSeguiti);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
