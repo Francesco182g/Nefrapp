fecdab6ed3bde5a888e42eddd200ca83de495fba
AnItalianMan <>
2019-12-18 20:55:40 +0100

    Aggiunto metodo modificaPiano

--- src/control/GestionePianoTerapeutico.java
+++ src/control/GestionePianoTerapeutico.java
@@ -1,5 +1,8 @@
 package control;
 
+import java.time.LocalDate;
+import java.util.regex.Pattern;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,13 +35,14 @@ public class GestionePianoTerapeutico extends HttpServlet {
 			String operazione = request.getParameter("operazione");
 			
 			if(operazione.equals("visualizza")) {
-				visualizzaPiano(request, response);
+				visualizzaPiano(request);
 				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/visualizzaPianoTerapeutico.jsp");
 				requestDispatcher.forward(request, response);
 			}
 			
 			else if(operazione.equals("modifica")) {
-				modificaPiano(request, response);
+				modificaPiano(request);
+				response.sendRedirect(request.getContextPath() + "/dashboard.jsp");
 			}
 			
 			//prova, TODO eliminare
@@ -64,7 +68,7 @@ public class GestionePianoTerapeutico extends HttpServlet {
 	 * 
 	 * @author Domenico Musone
 	 */
-	private void visualizzaPiano(HttpServletRequest request, HttpServletResponse response) {
+	private void visualizzaPiano(HttpServletRequest request) {
 		PianoTerapeutico pianoTerapeutico = null;
 		String codiceFiscalePaziente = request.getParameter("CFPaziente");
 		
@@ -72,7 +76,17 @@ public class GestionePianoTerapeutico extends HttpServlet {
 		request.setAttribute("pianoTerapeutico", pianoTerapeutico);
 	}
 	
-	private void modificaPiano(HttpServletRequest request, HttpServletResponse response) {
-		
+	private void modificaPiano(HttpServletRequest request) {
+		final String REGEX_DATA = "^(0[1-9]|1[0-9]|2[0-9]|3[01])/(0[1-9]|1[012])/[0-9]{4}$";
+		String dataFine = request.getParameter("dataFine");
+		if(Pattern.matches(REGEX_DATA, dataFine)) {
+			String codiceFiscalePaziente = request.getParameter("CFPaziente");
+			String diagnosi = request.getParameter("diagnosi");
+			String farmaci = request.getParameter("farmaci");
+			LocalDate dataFineTerapia  = LocalDate.parse(dataFine);
+			PianoTerapeuticoModel.updatePianoTerapeutico(new PianoTerapeutico(codiceFiscalePaziente, diagnosi, farmaci, dataFineTerapia));
+		}else {
+			//TODO ritorna messaggio errore
+		}
 	}
 }
