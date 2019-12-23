9146b0f6fe1dc399892e11f6ab894d4b76464c8a
Sara <>
2019-12-23 22:40:00 +0100

    Agiunto campo di non accesso

--- src/control/GestioneNotifica.java
+++ src/control/GestioneNotifica.java
@@ -19,51 +19,56 @@ import model.PianoTerapeuticoModel;
 @WebServlet("/GestioneNotifica")
 public class GestioneNotifica extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GestioneNotifica() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public GestioneNotifica() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		Utente utente = null;
 		HttpSession session = request.getSession();
 		utente = (Utente) session.getAttribute("utente");
 		if ((boolean) session.getAttribute("accessDone") == true) {
-			if (session.getAttribute("isPaziente")!=null && (boolean)session.getAttribute("isPaziente")==true) {
-				//Fai i controlli per il paziente (messaggio, annuncio, piano terapeutico)
-				int nm=MessaggioModel.countMessaggiNonLetti(utente.getCodiceFiscale());
-				int na=AnnuncioModel.countAnnunciNonLetti(utente.getCodiceFiscale());
-				boolean pt=PianoTerapeuticoModel.isPianoTerapeuticoVisualizzato(utente.getCodiceFiscale());
-				if(pt) { 
+			if (session.getAttribute("isPaziente") != null && (boolean) session.getAttribute("isPaziente") == true) {
+				// Fai i controlli per il paziente (messaggio, annuncio, piano terapeutico)
+				int nm = MessaggioModel.countMessaggiNonLetti(utente.getCodiceFiscale());
+				int na = AnnuncioModel.countAnnunciNonLetti(utente.getCodiceFiscale());
+				boolean pt = PianoTerapeuticoModel.isPianoTerapeuticoVisualizzato(utente.getCodiceFiscale());
+				if (pt) {
 					session.setAttribute("notificheAnnunci", na);
 					session.setAttribute("notificheMessaggi", nm);
 					session.setAttribute("notifichePianoTerapeutico", 1);
-				}
-				else {
+				} else {
 					session.setAttribute("notificheAnnunci", na);
 					session.setAttribute("notificheMessaggi", nm);
 				}
-			}
-			if (session.getAttribute("isMedico")!=null && (boolean)session.getAttribute("isMedico")==true) {
-				//Fai i controlli per il medico (messaggio)
-				int nm=MessaggioModel.countMessaggiNonLetti(utente.getCodiceFiscale());
+			} else if (session.getAttribute("isMedico") != null && (boolean) session.getAttribute("isMedico") == true) {
+				// Fai i controlli per il medico (messaggio)
+				int nm = MessaggioModel.countMessaggiNonLetti(utente.getCodiceFiscale());
 				session.setAttribute("notificheMessaggi", nm);
 			}
+		} else {
+			System.out.println("L'utente deve essere loggato");
+
 		}
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
