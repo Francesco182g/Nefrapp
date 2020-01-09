cb1d29dec924e879af123547a2b32c88fad28f88
PuddiPuddi96 <>
2020-01-10 00:31:56 +0100

    Finiti test per MedicoModel e UtenteModel. TODO: implementare un caso di test segnalato all'interno della classe.

--- src/test/model/MedicoModelTest.java
+++ src/test/model/MedicoModelTest.java
@@ -3,6 +3,7 @@ package test.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,11 +17,16 @@ class MedicoModelTest {
 	private static final String residenzaMedico = "Via Roma, 22, Scafati, 80030, NA";
 	
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
+		Medico medico = new Medico("M", residenzaMedico, dataNascitaMedico, 
+				"GRMBNN67L11B519R", "Geremia", "Bernini", "G.Bernini67@gmail.com", "Campobasso");
+		MedicoModel.addMedico(medico, "Quadri1234");
 	}
-
+	
 	@AfterEach
-	void tearDown() throws Exception {
+	void tearDown() {
+		MedicoModel.removeMedico("GRMBNN67L11B519R");
+		MedicoModel.removeMedico("CRLBNN67L11B519R");
 	}
 	
 	@Test
@@ -59,15 +65,13 @@ class MedicoModelTest {
 		
 	}
 	
-	//TODO riaggiungere il medico rimosso
-	
 	@Test
 	void testAddMedico() {
 		Medico daAggiungere = new Medico("M", residenzaMedico, dataNascitaMedico, 
 				"CRLBNN67L11B519R", "Carlo", "Bernini", "C.Bernini67@gmail.com", "Campobasso");
 		MedicoModel.addMedico(daAggiungere, "Quadri9999");
 		
-		Medico medico = MedicoModel.getMedicoByCFPassword("GRMBNN67L11B519R", "Quadri1234");
+		Medico medico = MedicoModel.getMedicoByCFPassword("CRLBNN67L11B519R", "Quadri9999");
 		assertNotNull(medico);
 		assertEquals(medico.getNome(), "Carlo");
 		assertEquals(medico.getCognome(), "Bernini");
@@ -81,31 +85,52 @@ class MedicoModelTest {
 		MedicoModel.removeMedico(daAggiungere.getCodiceFiscale());
 	}
 	
-	//TODO da finire
-
 	@Test
 	void testGetAllMedici() {
-		fail("Not yet implemented");
+		List<Medico> medici = MedicoModel.getAllMedici();
+		assertNotNull(medici);
+		//TODO: non so se bisogna fare ulteriori controlli sui medici trovati
 	}
 
 	@Test
 	void testUpdateMedico() {
-		fail("Not yet implemented");
+		Medico daAggiornare = MedicoModel.getMedicoByCF("GRMBNN67L11B519R");
+		assertNotNull(daAggiornare);
+		daAggiornare.setNome("Geremia Luca");
+		daAggiornare.setEmail("G.Bernini1967@gmail.com");
+		MedicoModel.updateMedico(daAggiornare);
+		daAggiornare = MedicoModel.getMedicoByCF("GRMBNN67L11B519R");
+		assertNotNull(daAggiornare);
+		assertEquals(daAggiornare.getNome(), "Geremia Luca");
+		assertEquals(daAggiornare.getEmail(), "G.Bernini1967@gmail.com");
 	}
 
 	@Test
 	void testGetMediciByPazienteSeguito() {
-		fail("Not yet implemented");
+		//List<Medico> medici = MedicoModel.getMediciByPazienteSeguito(codiciFiscaliMedici)
+		//assertNotNull(medici);
+		//TODO: non so se bisogna fare ulteriori controlli sui medici trovati
+
 	}
 
 	@Test
 	void testGetMedicoByEmail() {
-		fail("Not yet implemented");
+		Medico medico = MedicoModel.getMedicoByEmail("G.Bernini67@gmail.com");
+		assertNotNull(medico);
+		assertEquals(medico.getNome(), "Geremia");
+		assertEquals(medico.getCognome(), "Bernini");
+		assertEquals(medico.getEmail(), "G.Bernini67@gmail.com");
+		assertEquals(medico.getCodiceFiscale(), "GRMBNN67L11B519R");
+		assertEquals(medico.getSesso(), "M");
+		assertEquals(medico.getResidenza(), residenzaMedico);
+		assertEquals(medico.getDataDiNascita(), dataNascitaMedico);
+		assertEquals(medico.getLuogoDiNascita(), "Campobasso");
 	}
 
 	@Test
 	void testUpdatePasswordMedico() {
-		fail("Not yet implemented");
+		MedicoModel.updatePasswordMedico("GRMBNN67L11B519R", "NonVoglioQuadri12");
+		Medico medico = MedicoModel.getMedicoByCFPassword("GRMBNN67L11B519R", "NonVoglioQuadri12");
+		assertNotNull(medico);
 	}
-
 }
---
+++ src/test/model/UtenteModelTest.java
@@ -0,0 +1,62 @@
+package test.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import bean.Medico;
+import bean.Paziente;
+import bean.Utente;
+import model.MedicoModel;
+import model.PazienteModel;
+import model.UtenteModel;
+
+class UtenteModelTest {
+	private static final LocalDate dataNascitaMedico = LocalDate.parse("1967-07-11");
+	private static final String residenzaMedico = "Via Roma, 22, Scafati, 80030, NA";
+	private static final String residenzaPaziente = "Via Mazzini, 22, Bellizzi, 84092, SA";
+	private static final LocalDate dataNascitaPaziente = LocalDate.parse("1979-05-16");
+
+	@BeforeEach
+	void setUp() {
+		Medico medico = new Medico("M", residenzaMedico, dataNascitaMedico, 
+				"GRMBNN67L11B519R", "Geremia", "Bernini", "G.Bernini67@gmail.com", "Campobasso");
+		MedicoModel.addMedico(medico, "Quadri1234");
+		
+		Paziente paziente = new Paziente("M", "RSSGPP79E16I483", "Giuseppe", "Russo", "russo.giuseppe@live.it", residenzaPaziente, 
+											"Scafati", dataNascitaPaziente, true, new ArrayList<String>(Arrays.asList("GRMBNN67L11B519R")));
+		PazienteModel.addPaziente(paziente, "PasswordDifficile");
+	}
+	
+	@AfterEach
+	void tearDown() {
+		MedicoModel.removeMedico("GRMBNN67L11B519R");
+		PazienteModel.removePaziente("RSSGPP79E16I483");
+	}
+
+	@Test
+	void testGetUtenteByCFMedico() {
+		Utente utente = UtenteModel.getUtenteByCF("RSSGPP79E16I483");
+		assertNotNull(utente);
+		assertEquals(utente.getNome(), "Giuseppe");
+		assertEquals(utente.getCognome(), "Russo");
+		assertEquals(utente.getEmail(), "russo.giuseppe@live.it");
+		assertEquals(utente.getCodiceFiscale(), "RSSGPP79E16I483");
+	}
+	
+	@Test
+	void testGetUtenteByCFPaziente() {
+		Utente utente = UtenteModel.getUtenteByCF("GRMBNN67L11B519R");
+		assertNotNull(utente);
+		assertEquals(utente.getNome(), "Geremia");
+		assertEquals(utente.getCognome(), "Bernini");
+		assertEquals(utente.getEmail(), "G.Bernini67@gmail.com");
+		assertEquals(utente.getCodiceFiscale(), "GRMBNN67L11B519R");
+	}
+}
